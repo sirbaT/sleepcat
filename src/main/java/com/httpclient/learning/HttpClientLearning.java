6709b0b14e0f37d2c811fb8bbab8ccfac2cd4f06
王亚光 <yaguang.wang@17zuoye.com>
2016-11-18 14:16:38 +0800

    fix

--- src/main/java/com/httpclient/learning/HttpClientLearning.java
+++ src/main/java/com/httpclient/learning/HttpClientLearning.java
@@ -5,5 +5,5 @@ package com.httpclient.learning;
  * Created by yaguang.wang on 2016/11/18.
  */
 public class HttpClientLearning {
-    
+
 }
---
+++ src/main/java/com/httpclient/learning/MineTypes.java
@@ -0,0 +1,13 @@
+package com.httpclient.learning;
+
+/**
+ * MIME type constant definitions.
+ *
+ * Created by yaguang.wang on 2016/11/18.
+ */
+abstract public class MineTypes {
+
+    public static final String JSON = "application/json";
+
+    public static final String XML = "application/xml";
+}
