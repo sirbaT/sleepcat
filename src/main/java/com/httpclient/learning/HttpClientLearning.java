c9e1082a106cfaf5b4844d5894e8e32ccd48c722
王亚光 <yaguang.wang@17zuoye.com>
2016-11-18 13:14:35 +0800

    fix

---
+++ src/main/java/com/httpclient/learning/HttpClientLearning.java
@@ -0,0 +1,9 @@
+package com.httpclient.learning;
+
+/**
+ * learning HttpClient api
+ * Created by yaguang.wang on 2016/11/18.
+ */
+public class HttpClientLearning {
+    
+}
---
+++ src/main/java/com/xiaoyadecaihong/sleepcat/Server.java
@@ -0,0 +1,11 @@
+package com.xiaoyadecaihong.sleepcat;
+
+/**
+ * open server
+ * Created by yaguang.wang on 2016/11/18.
+ */
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("First project on web");
+    }
+}
