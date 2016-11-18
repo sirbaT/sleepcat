aefde830b92f31dc60e3b33f5c5257e744353f9b
王亚光 <yaguang.wang@17zuoye.com>
2016-11-18 14:55:14 +0800

    first example

--- src/main/java/com/httpclient/learning/HttpClientLearning.java
+++ src/main/java/com/httpclient/learning/HttpClientLearning.java
@@ -1,9 +1,44 @@
 package com.httpclient.learning;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 /**
  * learning HttpClient api
  * Created by yaguang.wang on 2016/11/18.
  */
 public class HttpClientLearning {
+    private CloseableHttpClient client = HttpClients.createDefault();
 
+    public String get(String httpUrl) throws IOException {
+        HttpGet httpGet = new HttpGet(httpUrl);
+        System.out.println("Executing request " + httpGet.getRequestLine());
+        try {
+            final ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+                public String handleResponse(HttpResponse httpResponse) throws IOException {
+                    int status = httpResponse.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = httpResponse.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status:" + status);
+                    }
+                }
+            };
+            return client.execute(httpGet, responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+            client.close();
+        }
+        System.out.println("---------------------------------------------------");
+        return "get is failed";
+    }
 }
--- src/main/java/com/xiaoyadecaihong/sleepcat/Server.java
+++ src/main/java/com/xiaoyadecaihong/sleepcat/Server.java
@@ -1,11 +1,17 @@
 package com.xiaoyadecaihong.sleepcat;
 
+import com.httpclient.learning.HttpClientLearning;
+
+import java.io.IOException;
+
 /**
  * open server
  * Created by yaguang.wang on 2016/11/18.
  */
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("First project on web");
+    public static void main(String[] args) throws IOException {
+        // System.out.println("First project on web");
+        HttpClientLearning learner = new HttpClientLearning();
+        System.out.println(learner.get("http://www.baidu.com"));
     }
 }
